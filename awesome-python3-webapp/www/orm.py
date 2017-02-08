e5d7605da85f0df8094f38544519bd00a15ad646
zhang <691259192@qq.com>
2017-02-08 18:31:08 +0800

    orm.py  comment

--- awesome-python3-webapp/www/orm.py
+++ awesome-python3-webapp/www/orm.py
@@ -96,7 +96,7 @@ class ModelMetaclass(type):
         # 排除Model类本身:
         if name=='Model':
             return type.__new__(cls, name, bases, attrs)
-        # 获取table名称:
+        # 获取table名称: None or 1 -> 1
         tableName = attrs.get('__table__', None) or name
         logging.info('found model: %s (table: %s)' % (name, tableName))
         # 获取所有的Field和主键名:
@@ -130,47 +130,93 @@ class ModelMetaclass(type):
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
 
-class Model(dict, metaclass=ModelMetaclass):
-
-    def __init__(self, **kw):
-        super(Model, self).__init__(**kw)
-
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-    def getValue(self, key):
-        return getattr(self, key, None)
-
-    def getValueOrDefault(self, key):
-        value = getattr(self, key, None)
-        if value is None:
-            field = self.__mappings__[key]
-            if field.default is not None:
-                value = field.default() if callable(field.default) else field.default
-                logging.debug('using default value for %s: %s' % (key, str(value)))
-                setattr(self, key, value)
-        return value
+# =====================================Model基类区==========================================
 
 
+# 编写元类
+class ModelMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        # 排除Model类本身
+        if name=='Model':
+            return type.__new__(cls, name, bases, attrs)
+        # 获取table名称
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+        # 获取所有定义域中的属性和主键
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('  found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+                # 先判断找到的映射是不是主键
+                if v.primary_key:
+                    if primaryKey:  # 若主键已存在,又找到一个主键,将报错,每张表有且仅有一个主键
+                        raise StandardError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        # 如果没有找到主键，也会报错
+        if not primaryKey:
+            raise StandardError('Primary key not found.')
+        # 定义域中的key值已经添加到fields里了，就要在attrs中删除，避免重名导致运行时错误
+        for k in mappings.keys():
+            attrs.pop(k)
+        # 将非主键的属性变形,放入escaped_fields中,方便sql语句的书写
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
+        attrs['__table__'] = tableName  # 表名
+        attrs['__primary_key__'] = primaryKey  # 主键属性名
+        attrs['__fields__'] = fields  # 除主键外的属性名
+        # 构造默认的SELECT, INSERT, UPDATE, DELETE语句
+        # 以下都是sql语句
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
 
+
+
+# =====================================Field定义域区==============================================
+# 首先来定义Field类，它负责保存数据库表的字段名和字段类型
 class Field(object):
-
+    # 定义域的初始化，包括属性（列）名，属性（列）的类型，主键，默认值
     def __init__(self, name, column_type, primary_key, default):
         self.name = name
         self.column_type = column_type
         self.primary_key = primary_key
         self.default = default
 
+    # 定制输出信息为 类名，列的类型，列名
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
 class StringField(Field):
-
+    # ddl是数据定义语言("data definition languages")，默认值是'varchar(100)'，意思是可变字符串，长度为100
+    # 和char相对应，char是固定长度，字符串长度不够会自动补齐，varchar则是多长就是多长，但最长不能超过规定长度
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        # ddl='varchar(100)'  映射为  self.column_type
         super().__init__(name, ddl, primary_key, default)
+
+
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
