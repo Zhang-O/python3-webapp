ff4222d2f18a5cd55d3765dcc64c4c0b526d5745
zhang <691259192@qq.com>
2017-02-08 15:43:45 +0800

    orm.py  comment

--- awesome-python3-webapp/www/orm.py
+++ awesome-python3-webapp/www/orm.py
@@ -3,29 +3,49 @@
 
 __author__ = 'Zhang'
 
+# aiomysql是Mysql的python异步驱动程序，操作数据库要用到
 import asyncio, logging, aiomysql
 
+
+# 这个函数的作用是输出信息，让你知道这个时间点程序在做什么
 def log(sql, args=()):
     logging.info('SQL: %s' % sql)
 
+# 创建全局连接池
+# 这个函数将来会在app.py的init函数中引用
+# 目的是为了让每个HTTP请求都能s从连接池中直接获取数据库连接
+# 避免了频繁关闭和打开数据库连接
 @asyncio.coroutine
 def create_pool(loop, **kw):
-    logging.info('create database connection pool...')
+    logging.info(' 常见数据库连接池... (create database connection pool...)')
     # log('create database connection pool...')
+
+    # 声明变量__pool是一个全局变量，如果不加声明，__pool就会被默认为一个私有变量，不能被其他函数引用
     global __pool
     __pool = yield from aiomysql.create_pool(
-        host=kw.get('host', 'localhost'),
-        port=kw.get('port', 3306),
-        user=kw['user'],
-        password=kw['password'],
-        db=kw['db'],
-        charset=kw.get('charset', 'utf8'),
-        autocommit=kw.get('autocommit', True),
-        maxsize=kw.get('maxsize', 10),
-        minsize=kw.get('minsize', 1),
-        loop=loop
+
+        # 下面就是创建数据库连接需要用到的一些参数，从**kw（关键字参数）中取出来
+        # kw.get的作用应该是，当没有传入参数是，默认参数就是get函数的第二项
+
+        host=kw.get('host', 'localhost'),  # 数据库服务器位置，默认设在本地
+        port=kw.get('port', 3306),  # mysql的端口，默认设为3306
+        user=kw['user'],  # 登陆用户名，通过关键词参数传进来。
+        password=kw['password'],  # 登陆密码，通过关键词参数传进来
+        db=kw['db'],  # 当前数据库名
+        charset=kw.get('charset', 'utf8'),  # 设置编码格式，默认为utf-8
+        autocommit=kw.get('autocommit', True),  # 自动提交模式，设置默认开启
+        maxsize=kw.get('maxsize', 10),  # 最大连接数默认设为10
+        minsize=kw.get('minsize', 1),  # 最小连接数，默认设为1，这样可以保证任何时候都会有一个数据库连接
+        loop=loop  # 传递消息循环对象，用于异步执行
     )
 
+
+# =================================以下是SQL函数处理区====================================
+# select和execute方法是实现其他Model类中SQL语句都经常要用的方法
+
+# 将执行SQL的代码封装仅select函数，调用的时候只要传入sql，和sql所需要的一些参数就好
+# sql参数即为sql语句，args表示要搜索的参数
+# size用于指定最大的查询数量，不指定将返回所有查询结果
 @asyncio.coroutine
 def select(sql, args, size=None):
     log(sql, args)
