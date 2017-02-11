00f6f13123256813a3e84d50d97638e67b44bdc9
zhang <691259192@qq.com>
2017-02-12 02:19:35 +0800

    config

--- awesome-python3-webapp/www/config_default.py
+++ awesome-python3-webapp/www/config_default.py
@@ -5,15 +5,13 @@
 Default configurations.
 '''
 
-__author__ = 'Michael Liao'
-
 configs = {
     'debug': True,
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'user': 'www',
-        'password': 'www',
+        'user': 'pyweb',
+        'password': 'pyweb',
         'db': 'awesome'
     },
     'session': {
--- awesome-python3-webapp/www/config_override.py
+++ awesome-python3-webapp/www/config_override.py
@@ -4,9 +4,6 @@
 '''
 Override configurations.
 '''
-
-__author__ = 'Michael Liao'
-
 configs = {
     'db': {
         'host': '127.0.0.1'
